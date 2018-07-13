cfadea07c6a7aa3353df9ec7eea41fd377e110f9
Fernando Longuini <flonguini@outlook.com>
2018-07-13 05:07:22 -0300

    First commit.

---
+++ SimpleICommand/ViewModel/Commands/HelloCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleICommand.ViewModel.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public ViewModelBase ViewModelBase { get; set; }
+
+        public HelloCommand(ViewModelBase viewModelBase)
+        {
+            ViewModelBase = viewModelBase;
+        }
+
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModelBase.SayHello();
+        }
+    }
+}
---
+++ SimpleICommand/ViewModel/Commands/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleICommand.ViewModel.Commands
+{
+    public class ViewModelBase
+    {
+        public Commands.HelloCommand HelloCommand { get; set; }
+
+        public ViewModelBase()
+        {
+            this.HelloCommand = new Commands.HelloCommand(this);
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show("Hello World.");
+        }
+    }
+}
